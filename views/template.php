361123c60ab69abc9bd7822324ef654ee13c4ae6
fredjacobbb <frdjacobbb@gmail.com>
2024-07-29 22:40:53 +0200

    .

--- views/template.php
+++ views/template.php
@@ -19,7 +19,7 @@
             <li class="fs-6"><a href="/"><i class="fas fa-home me-3"></i>Accueil</a></li>
             <li class="fs-6"><a href="/?q=schedules"><i class="fas fa-clock me-3"></i>Les horaires</a></li>
             <li class="fs-6"><a href="./?q=disciplines"><i class="fas fa-user-ninja me-3"></i>Les disciplines</a></li>
-            <?=  ?><li class="fs-6"><a href="./?q=login"><i class="fas fa-sign-in-alt me-3"></i>Se connecter</a></li>
+            <li class="fs-6"><a href="./?q=login"><i class="fas fa-sign-in-alt me-3"></i>Se connecter</a></li>
             <li class="fs-6"><a href="./?q=registration"><i class="fas fa-user-plus me-3"></i>S'inscrire</a></li>
             <li class="fs-6"><a href="./?q=contact-us"><i class="fas fa-envelope me-3"></i>Nous contacter</a></li>
         </ul>
