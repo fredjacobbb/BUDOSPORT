bfc043eafb22c4cabb021c76cdf4cdb7532ea5e1
fredjacobbb <frdjacobbb@gmail.com>
2024-08-01 16:01:28 +0200

    .

--- controllers/SchedulesController.php
+++ controllers/SchedulesController.php
@@ -15,43 +15,11 @@
             $this->schedulesList->ages = $this->usersModel->getAllAgesRanges();
             foreach ($this->schedulesList->ages as $key_age=>$age_range) {
                 $this->schedulesList->ages[$key_age]->disciplines = $this->disciplinesModel->getAllDisciplinesByAgeId($age_range->age_id);
-
-                // $this->schedulesList->ages[$age_range->age_tranche] = ['value' => $age_range->age_tranche];
-                foreach ($this->disciplinesModel->getAllDisciplines() as $key_discipline => $discipline) {
-                    $this->schedulesList->ages[$key_age]->disciplines[$key_discipline]->schedules = $this->schedulesModel->getSchedulesByAgeAndDiscipline($age_range->age_id, $discipline->discipline_id);
-
-                    // $this->schedulesList->ages[$age_range->age_tranche][$discipline->discipline_name] = ['value' => $discipline->discipline_name];
-                    foreach ($this->schedulesModel->getAllSchedules() as $schedule) {
-                        if ($schedule->age_id === $age_range->age_id) {
-                            if ($schedule->discipline_id === $discipline->discipline_id) {
-                                foreach ($this->days as $item_day => $day) {
-                                    if ($schedule->day === $item_day + 1) {
-                                        // $this->schedulesList->ages = (object) array('age' => $age_range->age_tranche);
-                                        // $this->schedulesList->ages->disciplines = (object) array('disicpline' => $discipline->discipline_name);
-                                        // $this->schedulesList->ages->disciplines->schedules[] = (object) array('schedule' => $schedule);
-                                        // $this->schedulesListAll[] = $this->schedulesList;
-
-                                        
-
-
-                                        // $this->schedulesList->ages->disciplines = (object) array(
-                                        //     'discipline' => $discipline->discipline_name
-                                        // );
-
-                                        // $this->schedulesList->ages->disciplines->schedules = (object) array(
-                                        //     'schedule' => $schedule
-                                        // );
-
-                                        // $this->schedulesListAll[] = $this->schedulesList;
-                                        
-                                    }
-                                }
-                            }
-                        }
-                    }
+                foreach ($this->schedulesList->ages[$key_age]->disciplines as $key_discipline => $discipline) {
+                    $this->schedulesList->ages[$key_age]->disciplines[$key_discipline]->schedules = $this->schedulesModel->getSchedulesByAgeAndDiscipline($age_range->age_id, $discipline->discipline_id);  
                 }
             }
-            var_dump($this->schedulesList);die;
+            return $this->schedulesList;
         }
 
     }
